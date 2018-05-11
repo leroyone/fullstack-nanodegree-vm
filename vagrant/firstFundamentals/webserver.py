502e22f0bd14ff9be956ea4f4e2be1878ffffa68
Leroy <thealloriginal@hotmail.com>
2018-05-12 02:10:54 +0900

    Before copy and pasting

--- vagrant/firstFundamentals/webserver.py
+++ vagrant/firstFundamentals/webserver.py
@@ -1,6 +1,15 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from databaseSetup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 class webserverHandler(BaseHTTPRequestHandler):
     """docstring for webserverHandler"""
     def do_GET(self):
@@ -18,6 +27,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print output
                 return
+
             if self.path.endswith("/hola"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -32,11 +42,59 @@ class webserverHandler(BaseHTTPRequestHandler):
                 print output
                 return
 
+            if self.path.endswith("/restaurants"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                rests = session.query(Restaurant).all()
+                output = ""
+                output += "<html><body>"
+                output += "<a href='/restaurants/new'>Make a New Restaurant Here</a><br>"
+                output += "<p>"
+                for eachRest in rests:
+                    output += eachRest.name + "<br>"
+                    output += "<a href='#'>Edit</a><br>"
+                    output += "<a href='#'>Delete</a><br><br>"
+                output += "</p>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1> Make a New Restaurant </h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>"
+                output += "<input name='newRestaurantName' type='text' placeholder='Goobies'>"
+                output += "<input type='submit' value='Cdgssgsd'>"
+                output += "</form></body></html>"
+                self.wfile.write(output)
+                return
+
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path) 
 
     def do_POST(self):
         try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rifle, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    newRestaurant = Restaurant(name = messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+            '''
             self.send_response(301)
             self.end_headers()
 
@@ -54,6 +112,7 @@ class webserverHandler(BaseHTTPRequestHandler):
             output += "</body></html>"
             self.wfile.write(output)
             print output
+            '''
         except:
             pass
 
